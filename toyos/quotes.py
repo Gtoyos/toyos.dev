f165d3e21d111f168a34e9be8e4f3b7694c45252
Gtoyos <guillermo.toyos@fing.edu.uy>
2022-11-06 20:49:02 +0100

    q2
    Learning NLP :)

--- toyos/quotes.py
+++ toyos/quotes.py
@@ -1,5 +1,5 @@
 QUOTES = [
-    "If I have seen further it is by standing on the shoulders of Giants. -Issac Newton",
+    "If I have seen further it is by standing on the shoulders of Giants. - Issac Newton",
 
     "The most effective debugging tool is still careful thought, coupled with judiciously placed print statements. -Brian Kernighan",
     
@@ -7,5 +7,7 @@ QUOTES = [
 
     """What business of mine is the future? No doubt Seldon has foreseen it and prepared against it. There will be other crises
         in the time to come when money power has become as dead a force as religion is now. Let my successors solve those new
-        problems, as I have solved the one of today. - Foundation (Isaac Asimov)"""
+        problems, as I have solved the one of today. - Foundation (Isaac Asimov)""",
+    
+    "You shall know a word by the company it keeps. - John Rupert Firth"
 ];
